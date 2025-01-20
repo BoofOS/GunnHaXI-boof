56607da97ebf7e2e1c9da67c48bd4d0edc2a9ab2
Eason Lu <aelnosu@gmail.com>
2025-01-20 15:21:37 -0800

    Added luacpp as a dependency
    This commit add the luacpp library as a dependency.
    On debian, you will need the `liblua5.4-dev` apt pkg.
    On RHEL, you will need the `lua-libs` rpm pkg. (Untested)

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <argparse.hpp>
 #include <fmt/core.h>
+#include <LuaCpp.hpp>
 
 int main()
 {
-    fmt::print("Hello, World!\n");
+    fmt::print("fmt test Hello, World!\n");
+    LuaCpp::LuaContext lua;
+    try {
+        lua.CompileStringAndRun("print('The fastest way to start using lua in a project')");
+    }
+    catch (std::runtime_error& e)
+    {
+        std::cout << e.what() << '\n';
+    }
     return 0;
 }
