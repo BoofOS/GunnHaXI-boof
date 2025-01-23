2b06ec1d523be080e2f96c24669d3a2b4c45c64b
Eason Lu <aelnosu@gmail.com>
2025-01-22 16:33:06 -0800

    Improve build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 mkdir -p cmake-build-debug
 cd cmake-build-debug
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++ -DCMAKE_CXX_FLAGS="-std=c++17 -Wall -Wextra -Wpedantic" ..
-cmake --build .
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_CXX_FLAGS="-g -glldb  -std=c++17 -Wall -Wextra -Wpedantic" ..
+cmake --build . --parallel $(( $(nproc) - 1 ))
