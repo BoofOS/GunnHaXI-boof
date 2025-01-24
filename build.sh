0cdb1a5f4b96b7f625aed0da9c0a5b1288fdfcc0
Eason Lu <aelnosu@gmail.com>
2025-01-24 12:40:44 -0800

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 mkdir -p cmake-build-debug
 cd cmake-build-debug
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_CXX_FLAGS="-g -glldb  -std=c++17 -Wall -Wextra -Wpedantic" ..
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug ..
 cmake --build . --parallel $(( $(nproc) - 1 ))
