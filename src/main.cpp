dba85bcfc100b206d65f0c675473ab3e31de5479
OOF <business@rdstudios.org>
2025-01-25 15:09:43 -0800

    started argparse config

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,31 @@
+#include <LuaCpp.hpp>
 #include <argparse.hpp>
 #include <fmt/core.h>
-#include <LuaCpp.hpp>
 
-int main()
-{
-    fmt::print("fmt test Hello, World!\n");
-    LuaCpp::LuaContext lua;
-    try {
-        lua.CompileStringAndRun("print('The fastest way to start using lua in a project')");
-    }
-    catch (std::runtime_error& e)
-    {
-        std::cout << e.what() << '\n';
-    }
-    return 0;
+int main() {
+	argparse::ArgumentParser parser("boof");
+
+	argparse::ArgumentParser install("install");
+	argparse::ArgumentParser search("search");
+
+	parser.add_subparser(install);
+	parser.add_subparser(search);
+
+	try {
+		program.parse_args(argc, argv);
+	}
+	catch (const std::exception& err) {
+		std::cerr << err.what() << std::endl;
+		std::cerr << program;
+		return 1;
+	}
+	// fmt::print("fmt test Hello, World!\n");
+	// LuaCpp::LuaContext lua;
+	// try {
+		// lua.CompileStringAndRun(
+			// "print('The fastest way to start using lua in a project')");
+	// } catch (std::runtime_error &e) {
+		// std::cout << e.what() << '\n';
+	// }
+	return 0;
 }
