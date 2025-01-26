92bbac89919adb835a9a197f25e2e34e7b4377a0
OOF <business@rdstudios.org>
2025-01-25 17:29:38 -0800

    Added archive url to constants

--- src/constants.h
+++ src/constants.h
@@ -10,3 +10,5 @@ const std::string BOOF_STORE_PATH = BOOF_BASE_PATH + "/store";
 
 const std::string BOOF_PACKAGE_LIST = BOOF_BASE_PATH + "/Packages";
 const std::string BOOF_PACKAGE_META = BOOF_BASE_PATH + "/Metadata";
+
+const std::string BOOF_PACKAGE_URL = "https://archive.archlinux.org/packages/.all/index.0.xz";
