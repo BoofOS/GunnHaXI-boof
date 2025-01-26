4af33f9adfe0ac67b9d61058ab07c32d0e02934a
OOF <business@rdstudios.org>
2025-01-25 16:50:53 -0800

    Added constants

---
+++ src/constants.h
@@ -0,0 +1,12 @@
+//
+// Created by linux on 1/25/25.
+//
+
+#pragma once
+#include <string>
+
+const std::string BOOF_BASE_PATH = "/usr/boof";
+const std::string BOOF_STORE_PATH = BOOF_BASE_PATH + "/store";
+
+const std::string BOOF_PACKAGE_LIST = BOOF_BASE_PATH + "/Packages";
+const std::string BOOF_PACKAGE_META = BOOF_BASE_PATH + "/Metadata";
